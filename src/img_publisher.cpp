c0c996578b7988b5954d13e98c78a7e16e25471f
Ksenia <liar.liar.liar2311@gmail.com>
2019-04-08 17:19:54 +0300

    added header file

--- src/img_publisher.cpp
+++ src/img_publisher.cpp
@@ -8,70 +8,91 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include "std_msgs/Float64.h"
+#include "img_publisher.h"
+
+Img_stream::Img_stream()
+{
+  cam_pub = imgTrs.advertiseCamera("image_raw",1);
+}
+
+bool Img_stream::load_img(std::string path, std::string filter, std::vector<cv::Mat> &images)
+{ 
+  // FIND PARAMETER NAME I.E> Wheelbase
+ // nh.param(param_name, path, default_path);
+  cv::glob(path + filter, fn, false);
+  size_t count = fn.size();
+  bool b = false;
+  for (size_t i=0;i<count;i++)
+  {
+    cv::Mat src = cv::imread(fn[i]);
+    if (!src.data)
+      {
+        ROS_INFO_STREAM("PROBLEM LOADING IMAGES!");
+        b = false;
+      }
+    else
+    {
+      images.push_back(cv::imread(fn[i]));
+      if (i==count - 1)
+      b = true;
+    }
+    
+  }
+  return b;
+}
 
 
 int main(int argc, char** argv)
 {
         ros::init(argc,argv,"img_streaming");
-        ros::NodeHandle nh;
-        image_transport::ImageTransport imgTrs(nh);
-        image_transport::CameraPublisher cam_pub = imgTrs.advertiseCamera("image_raw",1);
-    
-        float dist[5] = {-0.011276, -0.002467, -0.004967, 0.033951, 0.000000};
-        float camera[9] = { 2677.117911, 0.000000, 937.740481, 0.000000, 2638.726872, 455.023139, 0.000000, 0.000000, 1.000000 };
-        float rect[9]= {1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000 };
-        float proj[12] = {2615.817139, 0.000000, 952.909396, 0.000000, 0.000000, 2657.769043, 453.327589, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000 };
-
-        std::vector<cv::String> fn;
-        std::string path;
-        if (nh.getParam ("path",path))
-        {
-          cv::glob(path,fn, false);
-        }
-        else
-        {
-           cv::glob("/home/neu/pylon_camera/Validation/AprilTags/27/*.jpg",fn, false);
-        }
         std::vector<cv::Mat> images;
-        size_t count = fn.size(); 
-        for (size_t i=0;i<count;i++)
-        images.push_back(cv::imread(fn[i]));
-        ros::Rate rate(10);
-        while (ros::ok())
+        Img_stream img_str;
+        
+        if (img_str.load_img("path","filter", images)) 
         {
-          for (std::vector<cv::Mat>::iterator it = images.begin(); it !=images.end(); it++)
-          {
-            std_msgs::Header header;
-            header.frame_id = "";
-            sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", *it).toImageMsg();
-            sensor_msgs::CameraInfoPtr cam_msg(new sensor_msgs::CameraInfo());
-            cam_msg->header.stamp = ros::Time::now();
-            cam_msg->header.frame_id = "basler";
-            cam_msg->height = 1024;
-            cam_msg->width = 1280;
-            cam_msg->distortion_model = "plumb_bob";
-            
-            for (int i=0; i<5; i++)
+             ros::Rate rate(10);
+            while (ros::ok())
             {
-              cam_msg->D[i] = dist[i];
-            }
-            
-            for(int i=0; i<9; i++)
-            { 
-              cam_msg->K[i] = camera[i];
-              cam_msg->R[i] = rect[i];
-            }
-            
-            for(int i=0; i<12; i++)
-            {
-                cam_msg->P[i] = proj[i];
-            }
-            cam_pub.publish(msg, cam_msg);
-            ros::Duration(1).sleep();
+                for (std::vector<cv::Mat>::iterator it = images.begin(); it !=images.end(); it++)
+              {
+                      std_msgs::Header header;
+                      header.frame_id = "";
+                      sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", *it).toImageMsg();
+                      sensor_msgs::CameraInfoPtr cam_msg(new sensor_msgs::CameraInfo());
+                      cam_msg->header.stamp = ros::Time::now();
+                      cam_msg->header.frame_id = "basler";
+                      cam_msg->height = 1024;
+                      cam_msg->width = 1280;
+                      cam_msg->distortion_model = "plumb_bob";
+                      
+                      for (int i=0; i<5; i++)
+                      {
+                        cam_msg->D[i] = img_str.dist[i];
+                      }
+                      
+                      for(int i=0; i<9; i++)
+                      { 
+                        cam_msg->K[i] = img_str.camera[i];
+                        cam_msg->R[i] = img_str.rect[i];
+                      }
+                      
+                      for(int i=0; i<12; i++)
+                      {
+                          cam_msg->P[i] = img_str.proj[i];
+                      }
+                      img_str.cam_pub.publish(msg, cam_msg);
+                      ros::Duration(1).sleep();
+                    }
+                    ros::spinOnce();
+                    rate.sleep();
+                }   
+
           }
-          ros::spinOnce();
-          rate.sleep();
+        else 
+        {
+          ROS_INFO_STREAM("Failed to load images. Exit program");
         }
+
 
         return 0;
 }
---
+++ src/img_publisher.h
@@ -0,0 +1,39 @@
+#ifndef IMG_PUBLISHER
+#define IMG_PUBLISHER
+
+#include "ros/ros.h"
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include "ros/package.h"
+
+class Img_stream {
+    public:
+    Img_stream();
+    const float dist[5] = {-0.011276, -0.002467, -0.004967, 0.033951, 0.000000};
+    const float camera[9] = { 2677.117911, 0.000000, 937.740481, 0.000000, 2638.726872, 455.023139, 0.000000, 0.000000, 1.000000 };
+    const float rect[9]= {1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000 };
+    const float proj[12] = {2615.817139, 0.000000, 952.909396, 0.000000, 0.000000, 2657.769043, 453.327589, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000 };
+    const std::string default_filter = "*.jpg";
+    std::string default_path = ros::package::getPath("img_publisher") + "test/";
+    std::vector<cv::String> fn;
+    bool load_img(std::string path, std::string filter, std::vector<cv::Mat> &images);
+    image_transport::CameraPublisher cam_pub; 
+
+
+    
+    private:
+    ros::NodeHandle nh;
+    image_transport::ImageTransport imgTrs;
+    
+   
+
+};
+
+
+
+
+
+
+
+#endif // IMG_PUBLISHER
